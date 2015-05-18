8913daf542192b96cd9301538d52aa606226ac7a
Patrick Himmelmann <himmelmannp@googlemail.com>
2015-05-18 19:09:22 +0200

    Initial commit

---
+++ RFID-Reader.ino
@@ -0,0 +1,89 @@
+/*
+ * ----------------------------------------------------------------------------
+ * This is a MFRC522 library example; see https://github.com/miguelbalboa/rfid
+ * for further details and other examples.
+ * 
+ * NOTE: The library file MFRC522.h has a lot of useful info. Please read it.
+ * 
+ * Released into the public domain.
+ * ----------------------------------------------------------------------------
+ *
+ * Pin layout used:
+ * -------------------------------------
+ * Signal      MFRC522      Arduino    
+ * -------------------------------------
+ * RST/Reset   RST          3          
+ * SPI SS      SDA(SS)      9          
+ * SPI MOSI    MOSI         11 / ICSP-4 
+ * SPI MISO    MISO         12 / ICSP-1 
+ * SPI SCK     SCK          13 / ICSP-3 
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include "fabnfc.h"
+
+#define RST_PIN		3
+#define SS_PIN		9
+#define BUZZ_PIN  5
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create MFRC522 instance
+FabNFC fabnfc(mfrc522);
+
+void setup() {
+  pinMode(BUZZ_PIN,OUTPUT);
+  Serial.begin(9600);		// Initialize serial communications with the PC
+  while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();			// Init SPI bus
+  mfrc522.PCD_Init();		// Init MFRC522
+  Serial.println(F("Running, waiting for card..."));
+}
+
+void loop() {
+
+  int res=fabnfc.identify();
+  if (!(res==3 || res==0)) return;
+
+  if (res==3) { // magic number 0xFA 0xB1 not present
+    fabnfc.write();
+    return;
+  }
+
+  Serial.print(F("UID:"));
+  dump_byte_array(fabnfc.uid,7);
+  Serial.println();
+
+  if(fabnfc.pw_needed()){
+    Serial.println(fabnfc.pw_auth());
+  }
+
+  // write stuffs
+  fabnfc.use_type=FabNFC::ACCESS;
+  fabnfc.tag_type=FabNFC::STICKER;
+  
+
+  fabnfc.data[0]=0x00;
+  fabnfc.data[1]=0x00;
+  fabnfc.data[2]=0x01;
+  fabnfc.data[3]=0x01;
+
+  fabnfc.datasize=4;
+  
+  fabnfc.write();
+
+  //fabnfc.pw_set();
+
+  
+
+  digitalWrite(BUZZ_PIN,HIGH);
+  delay(250);
+  digitalWrite(BUZZ_PIN,LOW);
+
+}
+
+void dump_byte_array(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
---
+++ fabnfc.cpp
@@ -0,0 +1,124 @@
+#include "fabnfc.h"
+
+FabNFC::FabNFC(MFRC522& NFC):nfc(NFC),datasize(0){
+}
+
+byte FabNFC::identify(){
+	if (!nfc.PICC_IsNewCardPresent()){
+		return 1;
+	}
+
+	// is card's UID 7 Byte?
+	nfc.PICC_ReadCardSerial();
+	if (nfc.uid.size!=7){
+		return 2;
+	}
+
+	// is this card the same as last card?
+	if (memcmp(uid,nfc.uid.uidByte,7)==0){
+		return 0xff;
+	}
+
+	byte buffer[18];
+	byte size=18;
+
+	// check magic bytes for fablab card
+	nfc.MIFARE_Read(4,buffer,&size);
+	if (buffer[0]!=0xfa || buffer[1]!=0xb1){
+		return 3;
+	}
+	use_type=buffer[2];
+	tag_type=buffer[3];
+
+	// read CC (Capability Container) to identify chip
+	nfc.MIFARE_Read(3,buffer,&size);
+	chip=buffer[2];
+
+	// check if chip is supported
+	if(chip!=0x6d) {
+		return 4;
+	}
+	offset_confpages=0xe3;
+
+	for(byte i=0;i<7;i++){
+		uid[i]=nfc.uid.uidByte[i];
+	}
+	return 0;
+}
+
+void FabNFC::read(){
+	byte buffer[18];
+	byte size=sizeof(buffer);
+	nfc.MIFARE_Read(5,buffer,&size);
+	for(byte i=0;i<16;i++){
+		data[i]=buffer[i];
+	}
+}
+
+byte FabNFC::write(){
+	if(datasize>=100) return 1; 
+	byte buffer[4];
+	buffer[0]=0xfa;
+	buffer[1]=0xb1;
+	buffer[2]=use_type;
+	buffer[3]=tag_type;
+	nfc.MIFARE_Ultralight_Write(4,buffer,sizeof(buffer));
+
+	byte normsize=(datasize+4)>>2; // round to next multiple of 4 and divide by 4
+	data[datasize]=0;
+
+	for(byte block=0;block<normsize;block++){
+		for(byte i=0;i<4;i++){
+			buffer[i]=data[block*4+i];
+		}
+		nfc.MIFARE_Ultralight_Write(5+block,buffer,sizeof(buffer));
+	}
+	return 0;
+}
+
+void FabNFC::halt(){
+	nfc.PICC_HaltA();
+}
+
+byte FabNFC::pw_auth(){
+
+	byte buffer[]={
+		0x1b, // command for NTAG21x to auth
+		0xfa,0xb1,0xab,0xff	// password to send
+	};
+
+	return nfc.PCD_MIFARE_Transceive(buffer,sizeof(buffer),false)==2;
+	// 2 = maybe OK?
+	// 4 = timeout/wrong pw
+
+}
+
+void FabNFC::pw_set(){
+	// password
+	byte buffer[]={
+		0xfa,0xb1,0xab,0xff	// password to set
+	};
+	nfc.MIFARE_Ultralight_Write(offset_confpages+2,buffer,sizeof(buffer));
+
+	//set AUTH0 and ACCESS
+	byte readbuffer[18];
+	byte size=sizeof(readbuffer);
+	nfc.MIFARE_Read(offset_confpages,readbuffer,&size);
+	readbuffer[3]=0;
+	readbuffer[4]=0;
+	nfc.MIFARE_Ultralight_Write(offset_confpages,readbuffer,4);
+	nfc.MIFARE_Ultralight_Write(offset_confpages+1,readbuffer+4,4);
+}
+
+byte FabNFC::pw_needed(){
+	//read AUTH0
+	byte readbuffer[18];
+	byte size=sizeof(readbuffer);
+	nfc.MIFARE_Read(offset_confpages,readbuffer,&size);
+	if(readbuffer[3]<=30){
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
